bab991249440de0b41fe040672db77af1a7b9b9f
HenryCheng <1004502125@qq.com>
2019-08-01 21:26:27 +0800

    change demo

--- GCMarqueeViewDemo/ViewController.m
+++ GCMarqueeViewDemo/ViewController.m
@@ -32,27 +32,31 @@
         [items addObject:model];
     }
     
-    GCMarqueeView *view = [[GCMarqueeView alloc] initWithFrame:CGRectMake(0, 200, 300, 28) type:GCMarqueeDirectionTypeLTR];
+    CGFloat marqueeWidth = 250.0f;
+    CGFloat lead = (self.view.frame.size.width - marqueeWidth) / 2;
+
+    
+    GCMarqueeView *view = [[GCMarqueeView alloc] initWithFrame:CGRectMake(lead, 200, marqueeWidth, 28) type:GCMarqueeDirectionTypeLTR];
     view.backgroundColor = [UIColor redColor];
     view.items = items;
     [self.view addSubview:view];
     
-    GCMarqueeView *view2 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(0, 300, 300, 28) type:GCMarqueeDirectionTypeTTB];
+    GCMarqueeView *view2 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(lead, 300, marqueeWidth, 28) type:GCMarqueeDirectionTypeTTB];
     view2.backgroundColor = [UIColor greenColor];
     view2.items = items;
     [self.view addSubview:view2];
     
-    GCMarqueeView *view3 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(0, 400, 300, 28) type:GCMarqueeDirectionTypeRTL];
+    GCMarqueeView *view3 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(lead, 400, marqueeWidth, 28) type:GCMarqueeDirectionTypeRTL];
     view3.backgroundColor = [UIColor purpleColor];
     view3.items = items;
     [self.view addSubview:view3];
     
-    GCMarqueeView *view4 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(0, 500, 300, 28) type:GCMarqueeDirectionTypeBTT];
+    GCMarqueeView *view4 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(lead, 500, marqueeWidth, 28) type:GCMarqueeDirectionTypeBTT];
     view4.backgroundColor = [UIColor blueColor];
     view4.items = items;
     [self.view addSubview:view4];
     
-    GCMarqueeView *view5 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(0, 600, 300, 28) type:GCMarqueeDirectionTypeReverse];
+    GCMarqueeView *view5 = [[GCMarqueeView alloc] initWithFrame:CGRectMake(lead, 600, marqueeWidth, 28) type:GCMarqueeDirectionTypeReverse];
     view5.backgroundColor = [UIColor cyanColor];
     view5.items = items;
     [self.view addSubview:view5];
